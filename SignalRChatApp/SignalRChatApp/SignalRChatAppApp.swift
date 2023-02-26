d66d6d141305a2f3351ef85c81c0d69e27ea9f17
moozzyk <moozzyk@gmail.com>
2023-02-25 21:42:15 -0800

    Add SignalRChatApp project

---
+++ SignalRChatApp/SignalRChatApp/SignalRChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignalRChatAppApp.swift
+//  SignalRChatApp
+//
+//  Created by Pawel Kadluczka on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct SignalRChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
