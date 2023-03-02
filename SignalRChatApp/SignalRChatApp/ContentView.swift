95ccf584c204f636f488fed7edfb31a2049ea853
moozzyk <moozzyk@gmail.com>
2023-03-01 22:20:32 -0800

    Implement UI

--- SignalRChatApp/SignalRChatApp/ContentView.swift
+++ SignalRChatApp/SignalRChatApp/ContentView.swift
@@ -7,15 +7,51 @@
 
 import SwiftUI
 
+struct Message: Hashable {
+    let name: String
+    let text: String
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(name)
+        hasher.combine(text)
+    }
+}
+
 struct ContentView: View {
+    @State private var messages: [Message] = []
+    @State private var newMessage = ""
+    @State private var showNamePopup = true
+    @State private var name = ""
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            List(messages, id: \.self) {
+                Text("**\($0.name)**: \($0.text)")
+            }
+
+            HStack {
+                TextField("Type a message", text: $newMessage)
+                    .padding()
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+
+                Button(action: {
+                    messages.append(Message(name: name, text: newMessage))
+                }) {
+                    Text("Send")
+                }
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(Color.white)
+                .cornerRadius(10)
+            }
         }
         .padding()
+        .alert("Please enter your name", isPresented: $showNamePopup, actions: {
+            TextField("User Name", text: $name)
+            Button("OK", action: {
+                showNamePopup = false
+            })
+        })
     }
 }
 
